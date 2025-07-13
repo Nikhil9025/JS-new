729f2c6a75b018fbdcbde190c8a828e8c0793d8b
Nikhil9025 <nikhil.tisangikar@gmail.com>
2025-07-13 11:05:16 +0000

    Conversion Operations

---
+++ Basics/conversionop.js
@@ -0,0 +1,35 @@
+
+// now understand conversion .
+
+// let score = 23;
+// let score1 = "23abc";
+// let score2 = null;
+// let score3 = undefined;
+// let score4 = true;
+
+
+
+// let valuInNumber = Number (score1);
+// console.log(typeof valuInNumber);
+// console.log(valuInNumber);
+
+
+// console.log(typeof score);
+// console.log(typeof score1);
+// console.log(typeof score2);
+// console.log(typeof score3);
+// console.log(typeof score4);
+
+let isLoggedIn =1;
+let booleanisLoggedeIn = Boolean(isLoggedIn);
+console.log(booleanisLoggedeIn);
+
+// 1 => true ; 0 => false ;
+// ""  => false ;
+// "Hello"  => true ;
+
+
+
+// "23"  == > 23 .
+// "23abc"  == > NaN .
+//  true => 1; ,  false  => 0;
