c6a429a7df572487fc144fba3bb11609b97137be
Nikhil9025 <nikhil.tisangikar@gmail.com>
2025-07-13 10:28:13 +0000

    Variable and Data types

---
+++ Basics/datatype.js
@@ -0,0 +1,19 @@
+"use strict"; // trat all js code as  newer version .
+
+// alert(3+3); // we are using node js , not browser .
+
+// Data types .
+
+let name = "Nikhil Tisangikar"; // strings value
+let age  = 25 ;                 // number or integer value 
+let isLoggedIn = true;          // boolaen value 
+
+let city;
+
+console.table([name , age , isLoggedIn]);
+console.log(typeof city);
+console.log(typeof undefined);
+let empty = null;
+console.log(typeof empty);
+
+
---
+++ Basics/variable.js
@@ -0,0 +1,12 @@
+const accountId = 12345;
+let accounrtEmail = "nikhilcorp26@gmail.com" ;
+var accountPassword = "12345";
+let accountCity = "Pune";
+let accountState;
+
+// accountId = 2; // not allowed we cannot change const value .
+
+console.table([accountId ,accounrtEmail ,  accountPassword ,accountCity , accountState])
+
+// dont use var  never
+
