f0c1070e0e3a4f655989c0146c4b60a682c29e35
Nikhil9025 <nikhil.tisangikar@gmail.com>
2025-07-14 13:21:59 +0000

    Comparision

---
+++ Basics/comparision.js
@@ -0,0 +1,35 @@
+console.log(1 > 2);
+console.log(2 >= 1);
+console.log(2 < 1);      // these all are simple comparision
+console.log(2 == 1);
+console.log(2 !=1 );
+
+//---------------------------------------------------------------------//////
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);      // these all are problem comparision
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+//comparision are done .
+
+
+
+
+
+
+
+
+
+
+
+
