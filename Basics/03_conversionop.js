09e11037c4900074e61c14acfb616ce8f0472bd8
Nikhil9025 <nikhil.tisangikar@gmail.com>
2025-07-15 12:24:52 +0000

    Conversion Operation

---
+++ Basics/03_conversionop.js
@@ -0,0 +1,63 @@
+
+// now understand conversion .
+
+// let score = 23;
+// let score1 = "23abc";
+// let score2 = null;
+// let score3 = undefined;
+// let score4 = true;
+
+
+
+// let valuInNumber = Number (score1);
+// console.log(typeof valuInNumber);
+// console.log(valuInNumber);
+
+
+// console.log(typeof score);
+// console.log(typeof score1);
+// console.log(typeof score2);
+// console.log(typeof score3);
+// console.log(typeof score4);
+
+let isLoggedIn =1;
+let booleanisLoggedeIn = Boolean(isLoggedIn);
+console.log(booleanisLoggedeIn);
+
+// 1 => true ; 0 => false ;
+// ""  => false ;
+// "Hello"  => true ;
+
+
+
+// "23"  == > 23 .
+// "23abc"  == > NaN .
+//  true => 1; ,  false  => 0;
+
+//Operations
+
+let Num = 3;
+
+let negNum = -Num;
+
+console.log(negNum);
+console.log(typeof negNum);
+
+
+// Type conversion . 
+
+console.log("1" + 2);   //  o/p is 12 because "1" is string .
+console.log(1 + "2");   // o/p is 12 because "2" is string .
+console.log("1" + 2 + 2); // o/p is 122 because "1" is string . 
+console.log(2 + 1 + "1"); // o/p is 31 becuse "1" is string 
+ 
+// if the first position is in the string then it consider as whole string .
+// if the the first position is in the number then it is consider as number then strings .
+
+
+
+
+
+
+
+
