956eb85610340e2ded917b9cac0b46633bc67ed2
Nikhil9025 <nikhil.tisangikar@gmail.com>
2025-07-18 20:00:52 +0000

    Dates in javascript

---
+++ Basics/07_DateIn_JS.js
@@ -0,0 +1,17 @@
+
+// Dates in JavaSript . 
+
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2023 , 0 , 26);
+console.log(myCreatedDate);
+console.log(myCreatedDate.toString());
+
+
+// IMPORTANT MESSAGE LEARN AGAIN DATES IN DETAIL -----------###############///////////
