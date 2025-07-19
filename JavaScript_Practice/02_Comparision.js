1595e7e5caf3f8e1c4c160885ac4bac0d99a0bbd
Nikhil9025 <nikhil.tisangikar@gmail.com>
2025-07-19 20:27:29 +0000

    The Practice of Java script

---
+++ JavaScript_Practice/02_Comparision.js
@@ -0,0 +1,15 @@
+
+// comparision 
+
+console.log(10 > 20);
+console.log(10 < 20);
+console.log(10 == 10);
+console.log(10 === "10");
+console.log(null == 0);
+console.log(10 != 25);
+
+
+
+
+
+
---
+++ JavaScript_Practice/03_String.js
@@ -0,0 +1,30 @@
+
+// Strings Practice . 
+
+let str = "Hello King";
+let str2 = "you are worrier";
+
+console.log(`You know who are you ${str} and ${str2}`);
+
+let gameName = "Rockstar Games";
+
+console.log(`the lenght of a game is ${gameName.length}`);
+console.log(`now the string is uppar case ${gameName.toUpperCase()}`);
+console.log(`the character at index 5 is  = ${gameName.charAt(5)}`);
+console.log(`the index of 'G' is = ${gameName.indexOf("k")}`);
+console.log(`the substring from index 3 to 6 is  =  ${gameName.substring(3, 6)}`);
+console.log(`slice the string from index 3 to 8 is = ${gameName.slice(3, 8)}`);
+
+let city = " pune  ";
+console.log(`the city is ${city.trim()}`);
+
+let newCity = "Mumbaii";
+console.log(`the new city is ${newCity.replace("i", "y")}`);
+console.log(`the new city is ${newCity.replaceAll("i", "y")}`);
+
+let email = "nikhil@gmail.com";
+console.log(`the email is ${email.includes("gmail")}`);
+console.log(`the email is ${email.includes("Raajkumar")}`);
+
+
+
