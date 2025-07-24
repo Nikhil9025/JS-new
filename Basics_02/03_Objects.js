d8ebd4144e00cfda8fb25de93503aa3e58f19837
Nikhil9025 <nikhil.tisangikar@gmail.com>
2025-07-24 15:21:32 +0000

    Object Part 1

---
+++ Basics_02/03_Objects.js
@@ -0,0 +1,62 @@
+// Object . 
+
+// Object Litrels . 
+
+
+// also learn symbol .
+
+const mySymbol = Symbol("SYM");
+
+const User = {
+    Name: "Nikhil",
+    [mySymbol]: "SYM_@",
+    Age: 25,
+    Email: "nikhilcorp@gmail.com",
+    City: "Pune",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Tuesday", "Wednesday"],
+};
+
+// console.log(User);
+// console.log(User[mySymbol]);
+
+// console.log(typeof mySymbol); // symbol converstion .
+
+
+//----------------------------///////////////-----------------------////////////////////////---------------------
+
+
+// // changing the value inside the object .
+
+// User.Email = "nikhil.tisangikar@gmail.com";
+
+// console.log(User.Email);  // Email changed .
+
+// // Freezing the object .
+
+// Object.freeze(User); // This will freeze the object and prevent any changes to it. now we cannot change the properties of the object .
+
+
+
+User.greeting = function () {
+    console.log("Hello Worrier");
+}
+
+
+console.log(User.greeting()); // it shows undedefined .  // we will learn after some time .
+console.log(User.greeting); // This will print the greeting function. if it shows [Function (anonymous)] in O/P it Means function is not executed yet. it just give reference to the function.
+
+
+// Reference to the function is stored in the object.
+
+User.greetingtwo = function () {
+    console.log(`The Name Of A King Is  = ${this.Name} `);
+}
+
+console.log(User.greetingtwo()); // This will print the Name of the King. this is used to refer the object itself.
+
+
+
+
+
+
