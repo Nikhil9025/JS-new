8d25603e6a6e4d56ffc6d013841d68de896d5eb7
Nikhil9025 <nikhil.tisangikar@gmail.com>
2025-07-31 09:19:03 +0000

    All About Arrow Functions

--- Basics_03/03_Arrow.js
+++ Basics_03/03_Arrow.js
@@ -18,28 +18,28 @@ User.WelcomeMessage();
 User.UserName = "Raajkumaar";
 User.WelcomeMessage();
 
-console.log(this); // it gives the empty object .
+console.log(this); // it gives the empty object . it refers empty object . because no context in global .
 
 
 // Now Arrow functions .
 
-function fun1() {
+function funN() {
 
     let UserName1 = "Nikhil";
     console.log(this.UserName1);
 }
-fun1(); // we cannot acces this in function .
+funN(); // we cannot acces this in function .
 
 // we can write function in many type .
 
 // 1] 
 
-function fun1() {
+function fun() {
 
     let UserName1 = "Nikhil";
     console.log(this.UserName1);
 }
-fun1();
+fun();
 
 // 2]
 
@@ -48,11 +48,12 @@ const fun1 = function () {
     let UserName1 = "Nikhil";
     console.log(this.UserName1);
 }
+
 fun1();
 
 // 3]  Arrow Functions .
 
-const function1 = () => {
+const function1 = () => {      // this is explicit function .
     let UserName2 = "Nikhil";
     console.log(this.UserName2);
 }
@@ -69,7 +70,7 @@ let result = addtwo(20, 40);
 console.log(result);
 
 
-const addToo = (num1, num2) => {
+const addToo = (num1, num2) => {   
 
     return num1 + num2;
 }
@@ -78,16 +79,39 @@ console.log(addToo(3, 5));
 
 // we can run this arrow function in other way also . if we use {} braces the it is mandatory to use return; 
 
-const addThre = (num1, num2, num3) => num1 + num2 + num3;
+const addThre = (num1, num2, num3) => num1 + num2 + num3; // this is arrow Implicit function
 console.log(addThre(2, 5, 7));
 
 const addFour = (num1, num2, num3, num4) => (num1 + num2 + num3 + num4);
 console.log(addFour(2, 3, 4, 8));
-// in this both case we dont need to use return; also we called emplicit return.
+// in this both case we dont need to use return; also we called emplicit return. we will use mostly this emplicit function in React .
+
+// now understand how to return object in arrow emplicit function .
+
+const ObjInArrow = () => ({UserName:"Nikhil" , ID:5634 , City:"Pune"});
+console.log(ObjInArrow()); // object passed in arrow implicit function .
 
 
+// this in detail . 
 
+const User1 = {
 
+    UserNAme: "Nikhil",
+    Message: function () {
+
+        console.log(`Hello ${this.UserNAme} Welcome to NT World`);
+        // console.log(this);
+        
+    }
+}
+
+// Object.freeze(User);
+
+User1.Message();
+User1.UserNAme = "Raajkumar";
+User1.Message();
+
+console.log(this);
 
 
 
