cdd9fb46a3b655c2cfd468b8da277d8aabf865c6
Nikhil9025 <nikhil.tisangikar@gmail.com>
2025-08-03 11:06:47 +0000

    All About Control Flow

---
+++ Control_Flow_04/01_Basics.js
@@ -0,0 +1,55 @@
+// // Control Flow Or Logic Flow .
+
+// // comparision operator .
+// //  < , > , <= , >= , == , === , != , !== , 
+
+
+
+// 1st control flow is ( if ) .  
+
+// // Understand with Example .
+// Ex - 1]
+
+let Tempreture = 79;
+
+if (Tempreture <= 50) {
+
+    console.log("Tempreture is under 50");
+
+} else {
+    console.log("Tempreture is more than 50");
+
+}
+
+console.log("Executed "); // it will always run because is outside the if scope .
+
+// Ex - 02]
+
+const score = 200;
+
+if (score >= 100) {
+
+    let power = "Fly";
+    console.log(`The Powe of a user is = ${power}`);
+}
+
+//   console.log(`The Powe of a user is = ${power}`); if we acces this log outside the scope . it will show the error because we know we cant acces the scope value outside the scope .
+
+// Short hand notation.
+
+const balence = 300;
+
+if (balence >= 500) console.log("Account Approved"); // single line short hand property .
+
+if (balence <= 500) console.log("Account Declined"),
+    console.log("Minimun 500 Rs Balence Required for Approve"); // multiple line short hand property . please do not use this short hand properteis .
+
+// Nesting . or Multiple Condition .
+
+
+
+
+
+
+
+
