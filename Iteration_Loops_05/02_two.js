09e481c08bb806f2f04dad8d14530e6751d1a7c3
Nikhil9025 <nikhil.tisangikar@gmail.com>
2025-08-05 07:00:59 +0000

    While and do-While Loop .

---
+++ Iteration_Loops_05/02_two.js
@@ -0,0 +1,25 @@
+// while loop .
+
+let index = 0;
+while (index <= 10) {
+    console.log(`The value of Index is = ${index}`);
+    index = index + 2;
+}
+
+// while loop on Array .
+
+const myArray = ["SuperMan" , "Batman" , "Splash"];
+
+let arr = 0;
+while (arr < myArray.length) {
+    console.log(myArray[arr]);
+    arr++;  // arr = arr + 1;
+}
+
+// do while loop .
+
+let marks = 1;
+ do {
+    console.log(`My Marks ${marks}`);
+    marks++;
+ } while (marks <= 10);  // if the condition will false . it runs atleast once .
